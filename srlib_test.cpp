a8a5a28e33074ab525ee893b38ca7a2829b5eb3a
xihu1364 <husong1364@163.com>
2015-05-06 16:46:53 +0800

    添加demo

---
+++ srlib_test.cpp
@@ -0,0 +1,18 @@
+// srlib_test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ClassFactory.h"
+#include "IoManager.h"
+#include "Ini.h"
+#include "Lock.h"
+#include "MemoryPool.h"
+#include "Singleton.h"
+#include "SysLog.h"
+#include "crc32.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
